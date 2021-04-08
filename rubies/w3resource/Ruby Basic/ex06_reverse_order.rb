b06550bb67b071b9c001d8542f2a5d5ecd5dba4e
DJDrama <putzzang@naver.com>
2021-04-08 14:11:41 +0900

    Input String and show with reversed order

---
+++ rubies/w3resource/Ruby Basic/ex06_reverse_order.rb
@@ -0,0 +1,9 @@
+# 6. Write a Ruby program which accept the user's first and last name and print them in reverse order with a space between them.
+
+print 'Input your first name: '
+first_name = gets.chomp
+
+print 'Input your last name: '
+last_name = gets.chomp
+
+puts "Hello #{last_name} #{first_name}"
