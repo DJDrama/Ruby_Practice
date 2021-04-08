67a3f0fb624046473a4b09bf867a59df848044fb
DJDrama <putzzang@naver.com>
2021-04-08 13:29:55 +0900

    Ruby Version & Patch Level

---
+++ rubies/w3resource/Ruby Basic/ex01_version_and_patch_level.rb
@@ -0,0 +1,5 @@
+# 1. Write Ruby program to get ruby version with patch number.
+# Ruby Version: 2.4.2
+# Ruby Patch Level: 198
+puts "Ruby Version: " + RUBY_VERSION
+puts "Ruby Patch Level: " + RUBY_PATCHLEVEL.to_s
