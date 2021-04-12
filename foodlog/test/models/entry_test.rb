8aba5001fe6d5b33b13fbfb498f67be5e71e8dbd
DJDrama <putzzang@naver.com>
2021-04-13 00:15:02 +0900

    system test

--- foodlog/test/models/entry_test.rb
+++ foodlog/test/models/entry_test.rb
@@ -8,4 +8,22 @@ class EntryTest < ActiveSupport::TestCase
     entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 20, fats: 10, calories: 400)
     assert entry.save
   end
+
+  test "should not save entry without calories" do
+    entry = Entry.new(meal_type: "Breakfast", proteins: 20, fats: 10)
+    assert_not entry.save
+  end
+
+  test "should not save entry without carbohydrates" do
+  end
+
+  test "should not save entry without proteins" do
+  end
+
+  test "should not save entry without fats" do
+  end
+
+  test "should not save entry without meal_type" do
+  end
+
 end
